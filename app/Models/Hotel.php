d4bb186f0f1b111782950c0ae91d6e6c5cf1c3d1
rangga48 <ranggaputra681@gmail.com>
2024-08-23 21:29:21 +0700

    models

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Hotel extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'link_gmaps',
+        'city_id',
+        'country_id',
+        'address',
+        'star_level',
+    ];
+
+    public function city(){
+        return $this()->belongsTo(City::class);
+    }
+
+    public function country(){
+        return $this()->belongsTo(Country::class);
+    }
+
+    public function photo(){
+        return $this()->belongsTo(HotelPhoto::class);
+    }
+
+    public function rooms(){
+        return $this()->belongsTo(HotelRoom::class);
+    }
+
+    public function getLowestRoomPrice(){
+        $minPrice = $this->rooms()->min('price');
+        return $minPrice ?? 0;
+    }
 }
