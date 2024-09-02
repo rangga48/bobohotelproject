dbc414414f6e85c5acb04f7ea577b0df6f89f107
rangga48 <ranggaputra681@gmail.com>
2024-09-02 20:43:54 +0700

    .

--- resources/views/admin/cities/index.blade.php
+++ resources/views/admin/cities/index.blade.php
@@ -4,7 +4,8 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Manage Cities') }}
             </h2>
-            <a href="  " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+            <a href="{{ route('admin.cities.create') }}"
+                class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
                 Add New
             </a>
         </div>
@@ -31,10 +32,13 @@
                             </h3>
                         </div>
                         <div class="hidden md:flex flex-row items-center gap-x-3">
-                            <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                            <a href="{{ route('admin.cities.edit', $city) }}"
+                                class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
                                 Edit
                             </a>
-                            <form action=" " method="POST">
+                            <form action="{{ route('admin.cities.destroy', $city) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
                                 <button type="submit" class="font-bold py-4 px-6 bg-red-700 text-white rounded-full">
                                     Delete
                                 </button>
