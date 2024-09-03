7418eced1e3c243ad8ce32d76b93559ff36d0105
rangga48 <ranggaputra681@gmail.com>
2024-09-03 20:34:11 +0700

    country admin

--- resources/views/admin/countries/index.blade.php
+++ resources/views/admin/countries/index.blade.php
@@ -9,38 +9,43 @@
             </a>
         </div>
     </x-slot>
-    
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
- 
-                <div class="item-card flex flex-row justify-between items-center">
-                    <div class="flex flex-row items-center gap-x-3">
-                        <div class="flex flex-col">
-                            <p class="text-slate-500 text-sm">Name</p>
+
+                @forelse ($countries as $country)
+                    <div class="item-card flex flex-row justify-between items-center">
+                        <div class="flex flex-row items-center gap-x-3">
+                            <div class="flex flex-col">
+                                <p class="text-slate-500 text-sm">Name</p>
+                                <h3 class="text-indigo-950 text-xl font-bold">
+                                    {{ $country->name }}
+                                </h3>
+                            </div>
+                        </div>
+                        <div class="hidden md:flex flex-col">
+                            <p class="text-slate-500 text-sm">Date</p>
                             <h3 class="text-indigo-950 text-xl font-bold">
-                                asdsadsa
+                                {{ $country->created_at->format('M, d, Y') }}
                             </h3>
                         </div>
-                    </div> 
-                    <div  class="hidden md:flex flex-col">
-                        <p class="text-slate-500 text-sm">Date</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">
-                            asdsadasdsa
-                        </h3>
+                        <div class="hidden md:flex flex-row items-center gap-x-3">
+                            <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                                Edit
+                            </a>
+                            <form action=" " method="POST">
+                                <button type="submit" class="font-bold py-4 px-6 bg-red-700 text-white rounded-full">
+                                    Delete
+                                </button>
+                            </form>
+                        </div>
                     </div>
-                    <div class="hidden md:flex flex-row items-center gap-x-3">
-                        <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
-                            Edit
-                        </a>
-                        <form action=" " method="POST"> 
-                            <button type="submit" class="font-bold py-4 px-6 bg-red-700 text-white rounded-full">
-                                Delete
-                            </button>
-                        </form>
-                    </div>
-                </div> 
-                
+                @empty
+                    <p>Belum ada data terbaru</p>
+                @endforelse
+
+
 
             </div>
         </div>
